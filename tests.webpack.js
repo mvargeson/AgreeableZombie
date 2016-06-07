46d2a3b1f5c7e5c5bd2a869c94e6d0035e1d10a7
Jen Wong <jenjwong@gmail.com>
2016-06-07 10:50:47 -0700

    Revert "Implement Server and React Testing"

--- tests.webpack.js
+++
@@ -1,6 +0,0 @@
-// /**
-//  * https://webpack.github.io/docs/context.html
-//  * load each test file into the webpack context
-
-var context = require.context('./test/components', true, /-test/);
-context.keys().forEach(context);
