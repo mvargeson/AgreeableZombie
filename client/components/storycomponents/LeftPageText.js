348958ceba4d003958a5b272866c63b743d5ede4
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-06-11 15:40:16 -0700

    fix up some css

--- client/components/storycomponents/LeftPageText.js
+++ client/components/storycomponents/LeftPageText.js
@@ -4,7 +4,7 @@ const LeftPageText = (props) => {
   console.log ('bookData inside LeftPageText is - ', props.bookData);
   console.log ('bookTitle inside LeftPageText is - ', props.bookTitle);
   return (
-  <div className="col s6 orange lighten-5 page-text">
+  <div className="col s6 page-text">
     {props.bookData[props.pageCounter].image ? <img className="page-image" src={props.bookData[props.pageCounter].content} /> : <p className="page-words">{props.bookData[props.pageCounter].content}</p>}
      
       
